d719da57b26f74cc876b011bc558fef2e5dda988
AmirHossein Shirazi <ashzih2@gmail.com>
2024-04-27 09:25:16 +0330

    new bullet speed

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings:
         self.ship_speed = 1.5
         
         # bullet setting
-        self.bullet_speed = 1.0
+        self.bullet_speed = 5.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
